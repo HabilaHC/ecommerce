c1fe30b8247b4cd67e9c7c4ff3ff51db7b43c1ee
CCHabilaHC <andrehabila@gmail.com>
2020-03-30 00:48:25 -0300

    inicio do login login incompleto

--- index.php
+++ index.php
@@ -25,6 +25,18 @@ $app->get('/admin', function() {
 	$page->setTpl("index");
 
 });
+
+$app->get('/admin/login', function() {
+
+	$page = new PageAdmin([
+
+		"header" => false,
+		"footer" => false,
+	]);
+
+	$page->setTpl("login");
+
+});
 $app->run();
 
 ?>
