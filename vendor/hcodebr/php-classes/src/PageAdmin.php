f606bb4b40c7c2ba792159b05064574d04e7fb5f
CCHabilaHC <andrehabila@gmail.com>
2020-03-29 22:05:55 -0300

    criando page admin e tpl admin

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hcode;
+
+class PageAdmin extends Page {
+
+	public function __construct($opts = array(), $stp_dir = "/views/admin/")
+	{
+
+		parent::__construct($opts, $stp_dir);
+	}
+
+}
+?>
